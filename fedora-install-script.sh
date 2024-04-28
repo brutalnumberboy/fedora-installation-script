8a89d42910e3d1d8fd88b992b922167553443387
brutalnumberboy <133921480+brutalnumberboy@users.noreply.github.com>
2024-04-28 16:48:33 +0200

    Add files via upload

---
+++ fedora-install-script.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+#fedora workstation installation script
+#dos2unix fedora-script.sh!!!!!!!
+#===============#
+#improve dnf performance
+echo 'max_parallel_downloads=10
+fastestmirror=True' | sudo tee -a /etc/dnf/dnf.conf > /dev/null
+#===============#
+
+#===============#
+#remove gnome apps
+sudo dnf remove gnome-tour totem -y
+#===============#
+
+#===============#
+#upgrade the system
+sudo dnf upgrade -y 
+#===============#
+
+#===============#
+#add rpmfusion repos
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf groupupdate core -y
+sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
+sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
+sudo dnf groupupdate sound-and-video -y
+#===============#
+
+#===============#
+#install native packages 
+sudo dnf install akmods git neofetch pandoc htop mc texlive-scheme-basic wireguard-tools gnome-tweaks gnome-console gnome-themes-extra adw-gtk3-theme snapshot
+#===============#
+
+#===============#
+#remove gnome-boxes and install cockpit
+sudo dnf remove gnome-boxes -y
+sudo dnf install cockpit cockpit-machines libvirt virt-viewer -y
+sudo systemctl enable libvirtd
+#===============#
+
+#===============#
+#remove native ff and libreoffice packages to replace them with flatpaks
+sudo dnf remove firefox *libreoffice* cheese -y
+#===============#
+
+#===============#
+#swap nano to vim
+sudo dnf swap nano-default-editor vim-default-editor -y
+#===============#
+
+#===============#
+#install mullvad vpn
+sudo dnf config-manager --add-repo https://repository.mullvad.net/rpm/stable/mullvad.repo -y
+sudo dnf install mullvad-vpn -y
+#===============#
+
+#===============#
+#import vscode repo and install code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+sudo dnf install code -y
+#===============#
+
+#===============#
+#install flatpaks
+flatpak update -y
+flatpak install flathub org.chromium.Chromium org.mozilla.firefox org.libreoffice.LibreOffice com.bitwarden.desktop com.github.tchx84.Flatseal de.haeckerfelix.Fragments com.usebottles.bottles org.telegram.desktop com.spotify.Client org.gnome.World.PikaBackup io.github.giantpinkrobots.flatsweep
+com.github.micahflee.torbrowser-launcher com.github.wwmm.easyeffects io.mpv.Mpv com.discordapp.Discord -y
+#===============#
+
+#===============#
+#configure vim
+echo "set nocompatible
+filetype on
+filetype plugin indent on
+set title
+syntax on
+set number relativenumber
+set ts=4 sw=4" > ~/.vimrc
+#===============#
+
+#===============#
+#configure gnome settings
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click 'true' #enable tap-to-click
+gsettings set org.gnome.shell.app-switcher  current-workspace-only 'true' #switch between apps only on current workspace
+#===============#
+
+#===============#
+#install easyeffects loudness equalizer preset
+curl https://raw.githubusercontent.com/Digitalone1/EasyEffects-Presets/master/LoudnessEqualizer.json -o ~/.var/app/com.github.wwmm.easyeffects/config/easyeffects/output/LoudnessEqualizer.json
+#===============#
+
+#===============#
+#restart the system
+reboot
+#===============#
