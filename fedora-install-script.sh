a995c96834199c84cfd731de746ea2815d8599f9
brutalnumberboy <133921480+brutalnumberboy@users.noreply.github.com>
2024-06-30 10:29:24 +0200

    Update fedora-install-script.sh

--- fedora-install-script.sh
+++ fedora-install-script.sh
@@ -65,7 +65,7 @@ sudo dnf install code -y
 #===============#
 #install flatpaks
 flatpak update -y
-flatpak install flathub org.chromium.Chromium org.mozilla.firefox org.libreoffice.LibreOffice com.bitwarden.desktop com.github.tchx84.Flatseal de.haeckerfelix.Fragments com.usebottles.bottles org.telegram.desktop com.spotify.Client org.gnome.World.PikaBackuporg.torproject.torbrowser-launcher com.github.wwmm.easyeffects io.mpv.Mpv -y
+flatpak install flathub org.chromium.Chromium org.mozilla.firefox org.libreoffice.LibreOffice com.bitwarden.desktop com.github.tchx84.Flatseal de.haeckerfelix.Fragments com.usebottles.bottles org.telegram.desktop com.spotify.Client org.gnome.World.PikaBackup org.torproject.torbrowser-launcher com.github.wwmm.easyeffects io.mpv.Mpv -y
 #===============#
 
 #===============#
